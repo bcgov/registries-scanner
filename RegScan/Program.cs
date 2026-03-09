5284f9ab12b45f7f413405430d3043ae7f6ca9df
Grant <Grant.Graham@outlook.com>
2026-03-09 11:17:17 -0700

    Added security feature to encrypt the sensitive.config file upon program execution.

--- RegScan/Program.cs
+++ RegScan/Program.cs
@@ -14,6 +14,7 @@ namespace RegScan
         {            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            Security.EncryptExternalSection("appSettings");//Encrypt the appSettings section in App.config to secure sensitive information
             Application.Run(new frmMDIMain());
         }
     }
