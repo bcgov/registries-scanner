df2c21fe307d9bc963b75a3db2a6e4fed289aaf9
tafriese <taylor.friesen@gov.bc.ca>
2026-05-27 11:13:57 -0700

    update documentation DocumentApi and remove more API calls ScanningParameterApi

--- ApiScanner/ScanningParameterApi.cs
+++ ApiScanner/ScanningParameterApi.cs
@@ -7,16 +7,20 @@ namespace ApiScanner
     {      
         static public string get()
         {
-            string endpoint = "/doc/api/v1/scanning/parameters";
-            string resp = APIRequest.MakeKeyRequest(endpoint, Method.GET);
-            return resp;
+            // We arent controlling these setting per user, anything that is set (or got) by these
+            // methods will be overwritten as soon as a new user uses the application and makes any
+            // change. If that feature is desired in the future it should be implemented fully.
+            //string endpoint = "/doc/api/v1/scanning/parameters";
+            //string resp = APIRequest.MakeKeyRequest(endpoint, Method.GET);
+            return "";
         }
 
         static public string patch(object data)
-        {            
-            string endpoint = "/doc/api/v1/scanning/parameters";
-            string resp = APIRequest.MakeKeyRequest(data, endpoint, Method.PATCH);
-            return resp;
+        {
+            // See notes above
+            //string endpoint = "/doc/api/v1/scanning/parameters";
+            //string resp = APIRequest.MakeKeyRequest(data, endpoint, Method.PATCH);
+            return "";
         }   
                 
     }
