5b1fb7eb409ed729ab66a46180dd43742101475a
tafriese <taylor.friesen@gov.bc.ca>
2026-07-06 10:59:28 -0700

    update documentation and clean functions frmMDIMain

--- RegScan/frmMDIMain.cs
+++ RegScan/frmMDIMain.cs
@@ -1,62 +1,52 @@
-﻿using AsyncRequests;
-using PdfSharp.Drawing.BarCodes;
+﻿using AppConfiguration;
 using System;
-using System.Collections.Generic;
 using System.Windows.Forms;
 using Utilities;
 using Vintasoft.Twain;
 
 namespace RegScan
 {
+    /// <summary>
+    /// frmMDIMain is the main window of the application. 
+    /// This window controls 
+    ///   - child window (frmScannerDocument) launching and state
+    ///   - all operations from the menu bar
+    /// </summary>
+    /// <remarkss>
+    /// MDI -> Multiple Document Interface. An applicaiton that can have multiple child windows 
+    ///   showing their own documents.
+    /// </remarkss>
     public partial class frmMDIMain : Form
     {
 
         #region Fields
 
+        // The current child frmScannerDocument form 
         frmScannerDocument _scannerForm = null;
 
         #endregion
 
         public frmMDIMain()
-        {            
-            APIRequest api = new APIRequest();
-
+        {
             InitializeComponent();
-
-            // Get the application setting.
-            AppSettingObj aso = new AppSettingObj();
 
             // Register Twain SDK
             TwainGlobalSettings tgs = new TwainGlobalSettings();
-            tgs.Register(aso.TwainSDKUserName, aso.TwainSDKEmail, aso.TwainSDKKey);
+            tgs.Register(ConfigKeys.TWAINSDKUSERNAME, ConfigKeys.TWAINSDKEMAIL, ConfigKeys.TWAINSDKKEY);
         }
 
-
-        private void selectSourceToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                // Create a device manager and display the built in selection dialog
-                DeviceManager deviceManager = new DeviceManager();
-                
-                deviceManager.Open();
-                
-                deviceManager.ShowDefaultDeviceSelectionDialog();
-
-                // Close our device manger and ask scanning form to create a new one
-                // for its use to pick up on the new selected scanner.
-                deviceManager.Close();
-                _scannerForm.CreateTwainDeviceManager();
-            }
-            catch (Exception _Error)
-            {
-                string msg = "TWAIN Device unable to reset.";
-                UtilityObj.WriteLog(UtilityObj.error, msg + Environment.NewLine +
-                    _Error.ToString());
-                MessageBox.Show(_Error.Message + msg + " Close and open scanning application");
-            }
-        }
-
+        /// <summary>
+        /// Event handler for catching when a frmScannerDocument form closes. If the user indicates
+        /// that they would like to load a new session a new form is created, if not this form will
+        /// close and the applicaiton exits.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <remarks>
+        /// If the form is closed for any non-user related event (i.e. OS shutdown, TaskManager 
+        /// force close, etc) this function will not attempt to ask the user if they would like to
+        /// load a new session. -> The applicaiton will exit.
+        /// </remarks>
         public void ChildFormClosing(object sender, FormClosingEventArgs e)
         {
             // Ask the user if they would like to start a new scanning session.
@@ -79,6 +69,12 @@ namespace RegScan
 
         }
 
+        /// <summary>
+        /// Handles creating a new child form. If the form closes automatically catch it and 
+        /// process with ChildFormClosing(). 
+        /// </summary>
+        /// <param name="sender">Any control that launches a new child form</param>
+        /// <param name="e">Any even that launches a new child form</param>
         public void MDIMain_Load(object sender, EventArgs e)
         {
             _scannerForm = new frmScannerDocument();
@@ -89,6 +85,46 @@ namespace RegScan
             _scannerForm.Show();
         }
 
+        #region Menu Item Event Handlers
+
+        /// <summary>
+        /// Allow the user to select a scanning device from the list of available options.
+        /// </summary>
+        /// <param name="sender">Menu Item Bar</param>
+        /// <param name="e">Event -> User selecting "Select Source"</param>
+        private void selectSourceToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Create a device manager and display the built in selection dialog
+                DeviceManager deviceManager = new DeviceManager();
+                
+                deviceManager.Open();
+                deviceManager.ShowDefaultDeviceSelectionDialog();
+
+                // Close our device manger and ask scanning form to create a new one
+                // for its use to pick up on the new selected scanner.
+                deviceManager.Close();
+                _scannerForm.CreateTwainDeviceManager();
+            }
+            catch (Exception err)
+            {
+                string msg = "TWAIN Device unable to reset.";
+                UtilityObj.WriteLog(UtilityObj.error, msg + Environment.NewLine +
+                    err.ToString());
+                MessageBox.Show(err.Message + msg + " Close and open scanning application");
+            }
+        }
+
+        /// <summary>
+        /// Handles the Find menu item being selected. 
+        /// The frmEnterBarCode form will return a barcode string from the user, which then can be
+        /// used to search for a document record. If one is found the record information will be
+        /// displayed in the frmScannerDocument form. 
+        /// If there is an error finding the document show an error message to the user.
+        /// </summary>
+        /// <param name="sender">Menu Item Bar</param>
+        /// <param name="e">Event -> User selecting "Find"</param>
         private void findToolStripMenuItem_Click(object sender, EventArgs e)
         {
             // If there isnt a scanning form already open start one
@@ -105,11 +141,19 @@ namespace RegScan
             }
         }
 
+        /// <summary>
+        /// Used to open and display the About Form (frmAbout). Only called if the user selects the
+        /// "About" menu item from the menu bar. 
+        /// </summary>
+        /// <param name="sender">Menu Item Bar</param>
+        /// <param name="e">Event -> User selecting "About"</param>
         private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
         {
             var frm = new frmAbout();
             frm.ShowDialog();
             frm.Dispose();
         }
+
+        #endregion
     }
 }
