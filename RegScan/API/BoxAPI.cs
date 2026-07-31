422e24c076fb5d04155dfffb9c929093720408d1
tafriese <taylor.friesen@gov.bc.ca>
2026-07-31 08:41:23 -0700

    add boxAPI file

---
+++ RegScan/API/BoxAPI.cs
@@ -0,0 +1,19 @@
+﻿using PdfSharp.Drawing.BarCodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegScan
+{
+    public class BoxAPI
+    {
+        public static string GetAllBoxes()
+        {
+            string endpoint = "/doc/api/v1/scanning/boxes";
+
+            return APIRequest.MakeKeyRequest(endpoint, RestSharp.Method.Get);
+        }
+    }
+}
