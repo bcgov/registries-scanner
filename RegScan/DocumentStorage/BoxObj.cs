bd530de5c83c11f485818e4c3fcebbc0a3bb1624
tafriese <taylor.friesen@gov.bc.ca>
2026-07-31 08:41:05 -0700

    add physical storage classes to project

--- RegScan/DocumentStorage/BoxObj.cs
+++ RegScan/DocumentStorage/BoxObj.cs
@@ -8,18 +8,28 @@ namespace RegScan
 {
     public class BoxObj
     {
+        private static List<BoxObj> _boxes;
+
         private int _boxID;
         private DateTime _openedDate;
         private DateTime _closedDate;
         private int _pageCount;
         private AccessionNumberObj _accessionNumber;
         
+
+        public static List<BoxObj> Boxes { get { return _boxes; } }
         public int BoxId { get { return _boxID; } set { _boxID = value; } }
         public int BoxNumber { get { return _accessionNumber.BoxNumber; } }
         public DateTime OpendedDate { get { return _openedDate; } set { _openedDate = value; } }
         public DateTime ClosedDate { get { return _closedDate; } set { _closedDate = value; } }
         public int PageCount { get { return _pageCount; } set { _pageCount = value; } }
         public AccessionNumberObj AccessionNumber { get { return _accessionNumber; } }
+
+        public void LoadBoxes()
+        {
+            _boxes = new List<BoxObj>();
+
+        }
     }
 
 }
