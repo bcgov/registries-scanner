51213ed4d45be76db6662293fc4c363800b1b997
tafriese <taylor.friesen@gov.bc.ca>
2026-08-17 11:58:46 -0700

    overhaul boxObj to better fit use cases

--- RegScan/DocumentStorage/BoxObj.cs
+++ RegScan/DocumentStorage/BoxObj.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,28 +9,94 @@ namespace RegScan
 {
     public class BoxObj
     {
-        private static List<BoxObj> _boxes;
-
+        public DateTime DEFAULTDATE = new DateTime(1900, 1, 2);
         private int _boxID;
-        private DateTime _openedDate;
-        private DateTime _closedDate;
+        private int _boxNumber;
+        private DateTimeOffset _openedDate;
+        private DateTimeOffset _closedDate;
+        private bool _closedSet = false;
+        private bool _closed = false;
         private int _pageCount;
-        private AccessionNumberObj _accessionNumber;
-        
+        private int _sequenceNumber;
+        private int _scheduleNumber;
 
-        public static List<BoxObj> Boxes { get { return _boxes; } }
+        [JsonProperty("boxId")]
         public int BoxId { get { return _boxID; } set { _boxID = value; } }
-        public int BoxNumber { get { return _accessionNumber.BoxNumber; } }
-        public DateTime OpendedDate { get { return _openedDate; } set { _openedDate = value; } }
-        public DateTime ClosedDate { get { return _closedDate; } set { _closedDate = value; } }
-        public int PageCount { get { return _pageCount; } set { _pageCount = value; } }
-        public AccessionNumberObj AccessionNumber { get { return _accessionNumber; } }
-
-        public void LoadBoxes()
-        {
-            _boxes = new List<BoxObj>();
-
+        [JsonProperty("boxNumber")]
+        public int BoxNumber { get { return _boxNumber; } set { _boxNumber = value; } }
+        [JsonProperty("openedDate")]
+        public DateTimeOffset OpenedDate { 
+            get { return _openedDate; } set { _openedDate = value;  } 
         }
+        public string OpenedDateString
+        {
+            get { return _openedDate.ToString("MMM dd, yyyy"); }
+        }
+        [JsonProperty("closedDate")]
+        public DateTimeOffset ClosedDate { 
+            get { return _closedDate; } 
+            set { 
+                _closedDate = value;
+                SetClosed();
+            } 
+        }
+        public string ClosedDateString
+        {
+            get
+            {
+                if (!_closedSet)
+                    SetClosed();
+                return _closed? _closedDate.ToString("MMM dd, yyyy") : "N/A";
+            }
+        }
+        public bool IsClosed { get {
+                if (!_closedSet)
+                    SetClosed();
+                return _closed; 
+            } }
+        public string Status { get { return _closed ? "Closed" : "Open"; } }
+        [JsonProperty("pageCount")]
+        public int PageCount { get { return _pageCount; } set { _pageCount = value; } }
+        [JsonProperty("scheduleNumber")]
+        public int ScheduleNumber { 
+            get { return _scheduleNumber; } set { _scheduleNumber = value; }
+        }
+        [JsonProperty("sequenceNumber")]
+        public int SequenceNumber { 
+            get { return _sequenceNumber; } set { _sequenceNumber = value; }
+        }
+        public string AccessionNumber
+        {
+            get { 
+                return String.Concat(
+                    _sequenceNumber.ToString(), _scheduleNumber.ToString(), _boxNumber.ToString());
+            }
+        }
+        public string AccessionNumberDashes
+        {
+            get
+            {
+                return String.Concat(_sequenceNumber.ToString(), "-", 
+                    _scheduleNumber.ToString(), "-", _boxNumber.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Sets the closed status for the box. This is based off the boxes _closedDate
+        /// </summary>
+        /// <remarks>
+        /// There are 2 conditions that must be met for a box to be considered closed:
+        ///     - The closed date is not null
+        ///     - The closed date is greater (more recent) than the default date
+        /// Both must be true for the box to be closed.
+        /// </remarks>
+        public void SetClosed()
+        {
+            _closed = (_closedDate != null && _closedDate > DEFAULTDATE);
+            _closedSet = true;
+        }
+
+
     }
 
 }
