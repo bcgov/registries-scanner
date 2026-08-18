c1aad7571558f46233954097db31b39731d24350
tafriese <taylor.friesen@gov.bc.ca>
2026-08-18 13:10:23 -0700

    update Enter Barcode form -> enter Text. Add handling for checking entered values are ints

--- RegScan/UI/Forms/frmEnterText.Designer.cs
+++ RegScan/UI/Forms/frmEnterText.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace RegScan
 {
-    partial class frmEnterBarCode
+    partial class frmEnterText
     {
         /// <summary>
         /// Required designer variable.
@@ -28,28 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtBarCode = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.txtBox = new System.Windows.Forms.TextBox();
+            this.textLabel = new System.Windows.Forms.Label();
             this.btnOk = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // txtBarCode
+            // txtBox
             // 
-            this.txtBarCode.Location = new System.Drawing.Point(82, 11);
-            this.txtBarCode.Name = "txtBarCode";
-            this.txtBarCode.Size = new System.Drawing.Size(141, 20);
-            this.txtBarCode.TabIndex = 3;
+            this.txtBox.Location = new System.Drawing.Point(82, 11);
+            this.txtBox.Name = "txtBox";
+            this.txtBox.Size = new System.Drawing.Size(141, 20);
+            this.txtBox.TabIndex = 3;
             // 
-            // label1
+            // textLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 12);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(73, 17);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Barcode:";
+            this.textLabel.AutoSize = true;
+            this.textLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textLabel.Location = new System.Drawing.Point(3, 12);
+            this.textLabel.Name = "textLabel";
+            this.textLabel.Size = new System.Drawing.Size(39, 17);
+            this.textLabel.TabIndex = 2;
+            this.textLabel.Text = "text:";
             // 
             // btnOk
             // 
@@ -75,16 +75,16 @@
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
-            // frmEnterBarCode
+            // frmEnterText
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(238, 93);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnOk);
-            this.Controls.Add(this.txtBarCode);
-            this.Controls.Add(this.label1);
-            this.Name = "frmEnterBarCode";
+            this.Controls.Add(this.txtBox);
+            this.Controls.Add(this.textLabel);
+            this.Name = "frmEnterText";
             this.Text = "Manual Entry";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -93,8 +93,8 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox txtBarCode;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBox;
+        private System.Windows.Forms.Label textLabel;
         private System.Windows.Forms.Button btnOk;
         private System.Windows.Forms.Button btnCancel;
     }
---
+++ RegScan/UI/Forms/frmEnterText.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace RegScan
+{
+    /// <summary>
+    /// A form used to prompt a user to enter a new barcode string manually.
+    /// If the user cancles or the 
+    /// </summary>
+    public partial class frmEnterText : Form
+    {
+
+        private string _textString;
+        private int _enteredInt;
+        public string EnteredText { get { return _textString; } }
+        public int EnteredInt { get { return _enteredInt; } }
+
+        public frmEnterText(string labelText)
+        {
+            InitializeComponent();
+            textLabel.Text = labelText;
+        }
+
+        /// <summary>
+        /// Handles making a request to the user to manually enter a barcode and returns the entered value.
+        /// </summary>
+        /// <param name="message"> String displayed in the message box </param>
+        /// <returns> string of characters entered by the user </returns>
+        public static string ManualBarcode(string message)
+        {
+            string retString = null;
+            // Display window to user.
+            if (MessageBox.Show(message, "Missing/ Not Found Barcode", 
+                MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                // User has indicated they wish to enter a barcode manually. Display a new form.
+                var frm = new frmEnterText("Barcode:");
+                frm.ShowDialog();
+                retString = frm.EnteredText;
+            }
+
+            return retString;
+        }
+
+        /// <summary>
+        /// Handles making a request to the user to manually enter a batch ID and returns the entered value.
+        /// </summary>
+        /// <returns> string of characters entered by the user </returns>
+        public static int ManualBatchID()
+        {
+            // Display a new form to get manual Batch ID
+            var frm = new frmEnterText("Batch ID:");
+            frm.ShowDialog();
+
+            return frm.EnteredInt;
+        }
+
+        private bool ValidateInt()
+        {
+            bool isInt = false;
+            try
+            {
+                _enteredInt = Int32.Parse(_textString);
+                isInt = true;
+            }
+            catch (FormatException)
+            {
+                var msg = "Value must be numeric characters only (0-9). " +
+                    "Please try again or select cancel.";
+                MessageBox.Show(msg, "Value Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return isInt;
+        }
+
+        /// <summary>
+        /// Catch the event when the user selects "Okay" on the form.
+        /// Check if the entered barcode is not empty or null before setting _textString
+        /// </summary>
+        /// <param name="sender">btnOk</param>
+        /// <param name="e">User selecting Okay button</param>
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtBox.Text))
+            {
+                _textString = txtBox.Text;
+                // Check if the entered value is a number. This may need to be updated if this form
+                // is ever used to capture non numeric strings
+                if (ValidateInt())
+                {
+                    this.Close();
+                }
+                else
+                {
+                    // clear any values set
+                    _enteredInt = -1;
+                    _textString = "";
+                    txtBox.Text = "";
+                    return;
+
+                }
+                
+            }
+        }
+
+        /// <summary>
+        /// Handle the event that the user cancels the form. Ensure the _textString is an empty
+        /// string and close the form.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            _textString = "";
+            this.Close();
+        }
+    }
+}
