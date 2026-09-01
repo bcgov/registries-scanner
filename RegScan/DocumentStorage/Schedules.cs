4f5320e37b98de0d6acc2aa9051639e72e5e54af
tafriese <taylor.friesen@gov.bc.ca>
2026-09-01 10:17:43 -0700

    remove unused imports from files

--- RegScan/DocumentStorage/Schedules.cs
+++ RegScan/DocumentStorage/Schedules.cs
@@ -1,10 +1,6 @@
 ﻿using Newtonsoft.Json;
-using PdfSharp.Pdf.Content.Objects;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RegScan.DocumentStorage
 {
--- RegScan/UI/Forms/frmBoxSelect.cs
+++ RegScan/UI/Forms/frmBoxSelect.cs
@@ -1,7 +1,5 @@
-using RegScan.UI;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
 
