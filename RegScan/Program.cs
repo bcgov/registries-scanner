4958819d6d45dc1ae599323c24dff70c8e5d575f
tafriese <taylor.friesen@gov.bc.ca>
2026-09-17 15:41:19 -0700

    wrap program in try catch

--- RegScan/Program.cs
+++ RegScan/Program.cs
@@ -10,12 +10,22 @@ namespace RegScan
         /// </summary>
         [STAThread]
         static void Main()
-        {            
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            new AppConfiguration.ConfigKeys(); //Initialize the configuration keys to ensure they are loaded before any other operations
-            Security.EncryptExternalSection("appSettings");//Encrypt the appSettings section in App.config to secure sensitive information
-            Application.Run(new frmMDIMain());
+        {
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                new AppConfiguration.ConfigKeys(); //Initialize the configuration keys to ensure they are loaded before any other operations
+                Security.EncryptExternalSection("appSettings");//Encrypt the appSettings section in App.config to secure sensitive information
+                Application.Run(new frmMDIMain());
+            }
+            catch (Exception e)
+            {
+                string msg = "Hit an uncaught and unexpected error:\n" + e.Message + "\n\nIf this issue persists contact support.";
+                MessageBox.Show(msg, "Unexpected Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                UtilityObj.WriteLog(UtilityObj.error, "Hit unexpected error during runtime:\n" + e.ToString());
+            }
+            
         }
     }
 }
